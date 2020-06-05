1690470224ee1d1f34eaf853d9daac045aaf3561
quoctrinhtrong69 <connguoiao69@gmail.com>
2020-06-05 22:52:14 +0700

    add react hooks example

---
+++ ReactJS/React-Hooks/react-hooks-example/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="app">
+      <h1 className="app__title">Hello Hooks</h1>
+    </div>
+  );
+}
+
+export default App;
